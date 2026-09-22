9f2f9fd1791e78bbc6087790b5b58dcbfe9a28bf
Harsha Sabnani <sabnaniharsha7@gmail.com>
2026-09-22 19:49:00 +0530

    Updated BestTimeToBuySellStock Java with full runnable program and reorganized Java folder

---
+++ ARRAYS/JAVA/BestTimeToBuySellStock.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class BestTimeToBuySellStock {
+
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of days: ");
+        int n = sc.nextInt();
+
+        int[] prices = new int[n];
+
+        System.out.print("Enter stock prices: ");
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int answer = maxProfit(prices);
+
+        System.out.println("Maximum Profit = " + answer);
+
+        sc.close();
+    }
+}
